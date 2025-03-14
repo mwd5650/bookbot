15a63606a9e038b1535f5969eeb62264501fa96d
mwd5650 <matt@davishomestead.com>
2025-03-13 21:38:51 -0400

    Added code to sumite book file at command line. Also added code to sort the character count list.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import get_words
+from stats import get_words, num_of_characters,sort_chars
+import sys
+
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,8 +8,24 @@ def get_book_text(path_to_file):
     return book_text
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
-    word_count = get_words(book)
-    print(f"{word_count} words found in the document")
+    
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        print("============ BOOKBOT ============")
+        book = get_book_text(sys.argv[1])
+        word_count = get_words(book)
+        characters_in_book = num_of_characters(book)
+        sorted_chars = sort_chars(characters_in_book)
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {word_count} total words")
+        print("--------- Character Count -------")
+        for char in sorted_chars:
+            character = char["char"]
+            count = char["num"]
+            print(f"{character}: {count}")
+        print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,30 @@
 def get_words(book_text):
     words = book_text.split()
     return len(words)
+
+def num_of_characters(book_text):
+    character_counts = {}
+
+    for c in book_text:
+        lower = c.lower()
+        if lower in character_counts:
+            character_counts[lower] += 1
+        else:
+            character_counts[lower] = 1
+    
+    return character_counts
+
+def sort_on(dict):
+    return dict["num"]
+
+
+def sort_chars(chars):
+    sorted_chars = []
+    for char in chars:
+        my_dict = {}
+        my_dict["char"] = char
+        my_dict["num"] = chars[char]
+        sorted_chars.append(my_dict)
+    
+    sorted_chars.sort(reverse=True, key=sort_on)
+    return sorted_chars
